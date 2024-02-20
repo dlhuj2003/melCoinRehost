55e641c2741447e20ea1ecafab7fdfac6777080e
ikechukwu19 <bestielawhine@gmail.com>
2024-02-20 07:24:04 +0100

    loc changes

--- src/router/index.js
+++ src/router/index.js
@@ -243,6 +243,13 @@ const router = createRouter({
       ],
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 router.beforeEach(async (to, from, next) => {
